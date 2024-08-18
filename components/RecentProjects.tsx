f7ed62302c8c558736fdbf0076b3bfd3e877f87b
Mohammad Alsallal <malslal787@gmail.com>
2024-08-19 00:29:39 +0300

    Report a Bug Button

--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -4,7 +4,7 @@ import { FaLocationArrow } from "react-icons/fa6";
 
 const RecentProjects = () => {
   return (
-    <div className="py-20">
+    <div id="projects" className="py-20">
       <h1 className="heading">
         A small selection of{" "}
         <span className="text-purple">recent projects</span>
@@ -13,10 +13,10 @@ const RecentProjects = () => {
         {projects.map(({ id, title, des, img, iconLists, link }) => (
           <div
             key={id}
-            className="lg:min-h-[32.5rem] h-[25rem] flex items-center justify-center sm:w-96 w-[80vw]"
+            className="sm:h-[41rem] h-[32rem] lg:min-h-[32.5rem] h-[25rem] flex items-center justify-center sm:w-96 w-[80vw] sm:w-[570px]"
           >
             <PinContainer title={link} href={link}>
-              <div className="relative flex items-center justify-center sm:w-96 w-[80vw] overflow-hidden h-[20vh] lg:h-[30vh] mb-10">
+              <div className="relative flex items-center justify-center sm:w-[570px] w-[80vw] overflow-hidden h-[20vh] sm:h-[30vh] mb-10">
                 <div className="relative w-full h-full overflow-hidden lg:rounded-3xl bg-[#13162d]">
                   <img src="/bg.png" alt="bg-img" />
                 </div>
@@ -31,12 +31,15 @@ const RecentProjects = () => {
               <div className="flex items-start justify-between mt-7 mb-3">
                 <div className="flex items-center">
                   {iconLists.map((icon, index) => (
-                    <div key={icon} className="border border-white/[0.2]">
+                    <div
+                      key={icon}
+                      className="border border-white/[0.2] rounded-full"
+                      style={{ transform: `translateX(-${5 * index * 2}px)` }}
+                    >
                       <img
                         src={icon}
                         alt={icon}
                         className="p-2 rounded-full bg-black lg:w-10 lg:h-10 w-8 h-8 flex justify-center items-center"
-                        style={{ transform: `translateX(-${5 * index * 2}px)` }}
                       />
                     </div>
                   ))}
