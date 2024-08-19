7409132bb02d445c919d91fc855410d06ca5f4cd
Mohammad Alsallal <malslal787@gmail.com>
2024-08-19 21:52:06 +0300

    bug fixes

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,14 +4,17 @@ import { socialMedia } from "@/data";
 
 const Footer = () => {
   return (
-    <footer className="w-full pt-20 pb-10" id="contact">
-      <div className="w-full absolute left-0 -bottom-72 min-h-96">
+    <footer
+      className="w-full md:mb-5 mb-[100px] pb-10 overflow-hidden"
+      id="contact"
+    >
+      {/* <div className="w-full absolute left-0 -bottom-72 min-h-96">
         <img
           src="/footer-grid.svg"
           alt="grid"
           className="w-full h-full opacity-50"
         />
-      </div>
+      </div> */}
       <div className="flex flex-col items-center">
         <h1 className="heading lg:max-w-[45vw]">
           Ready to take <span className="text-purple">your</span> digital
@@ -39,7 +42,12 @@ const Footer = () => {
               key={profile.id}
               className="w-10 h-10 cursor-pointer flex justify-center items-center backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border brder-black-300"
             >
-              <img src={profile.img} alt={profile.id} width={20} height={20} />
+              <img
+                src={profile.img}
+                alt="social media"
+                width={20}
+                height={20}
+              />
             </div>
           ))}
         </div>
